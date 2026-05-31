aaa76237066d7d82a3e2fd175c92e79f17215180
Yeferson Stiven Sossa Monsalve <YSossa@leonisa.com>
2026-05-31 09:34:55 -0500

    subir

--- services/auth.ts
+++ services/auth.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export const SESSION_KEY = 'auth_session';
 
-const DEFAULT_API_URL = 'http://localhost:81/api';
+const DEFAULT_API_URL = 'https://api.elevaluador.com/api';
 export const API_URL = process.env.EXPO_PUBLIC_API_URL ?? DEFAULT_API_URL;
 const LOGIN_PATH = process.env.EXPO_PUBLIC_LOGIN_PATH ?? '/login';
 let isHandlingExpiredSession = false;
